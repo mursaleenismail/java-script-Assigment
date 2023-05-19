8cf70573188616eea513416fce78ddbe7ff11865
mursaleenismail <127970348+mursaleenismail@users.noreply.github.com>
2023-05-19 07:52:45 -0700

    Add files via upload

---
+++ asigmnt 2/app.js
@@ -0,0 +1,126 @@
+//chp 1//
+alert("Mursaleen");
+alert("Muhammad Ismail");
+alert("mursaleenismail2@gmail.com");
+alert("0323-2965720");
+alert("Password");
+alert("Are You Need to learn");
+
+   //chp 2//
+var a="Hackathon Day";
+alert (a);
+
+teamname="RCB";
+alert (teamname);
+ 
+//chp3//
+
+var caseQty=144;
+var num=9;
+var sum=caseQty+num;
+
+var total=100;
+var shipping =10;
+var orderTotal=total+shipping;
+
+console.log(orderTotal);
+
+
+//chp 4//
+var productcost=3.45;
+console.log (productcost);
+
+var nameOfband;
+console.log (nameOfband);
+
+var mursaleenIsmail;
+console.log (mursaleenIsmail);
+
+
+
+//chp 5//
+
+//Modulus %//
+let x = 20;
+let y = 6;
+let z = x % y;
+
+console.log (z);
+
+var largeNum=1000;
+var x =2000;
+
+var z =largeNum * x;
+console.log (z);
+
+var largeNum=1000*2000;
+console.log (largeNum);
+
+
+var variable1=60;
+var variable2=10;
+var result=variable1-variable2;
+
+console.log(result);
+
+
+var value1=40;
+var value2=2;
+var remainder=value1%value2;
+
+console.log(result);
+
+
+var number1= 6;
+var number2=30;
+
+var result=number1 * number2;
+
+alert ("The result of the Multiplication is + ")
+
+
+//chp 6//
+
+var x += 1;
+
+var x= 100;
+var x -= 1;
+console.log(x);
+
+var x = 50;
+var y = x++;
+
+console.log(y);
+
+
+//chp 7//
+
+var calculatedNum=2 + 2 * 6;
+console.log(calculatedNum);
+
+var calculatedNum2=(2 + 2) * 6;
+console.log(calculatedNum2);
+
+var calculatedNum3=(2 + 2) * (4 + 2);
+console.log(calculatedNum3);
+
+var calculatedNum4=(2 + 2) * 4 + 2;
+console.log(calculatedNum4);
+
+var calculatedNum5=(2 + 2) * (4 + 10);
+console.log(calculatedNum5);
+
+var calculatedNum6= 2 + 2 * 4 + 10;
+console.log(calculatedNum6);
+
+var calculatedNum7=4 / (2 * 4) ;
+console.log(calculatedNum7);
+
+
+//chp 8//
+
+
+
+
+
+
